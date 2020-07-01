cb78fa1227b1a4297cf38b18dcab950b667746e7
tahmid526628 <42012470+tahmid526628@users.noreply.github.com>
2020-07-01 17:03:29 +0600

    Update slideshow.js

--- js/slideshow.js
+++ js/slideshow.js
@@ -3,16 +3,25 @@ slide_show();
 
 function slide_show(){
     var images = document.getElementsByClassName("slide-images");
+    var dots = document.getElementsByClassName("dots");
     // console.log(images.length);
     //make all images invisible
     for(var i=0; i<images.length; i++){
         images[i].style.display = "none";
     }
+    //making default
+    for(var i=0; i<dots.length; i++){
+        // dots[i].style.background = "blueviolet"
+        dots[i].className = dots[i].className.replace(" dots-active", "");
+    }
 
     if(image_index >= images.length){
         image_index = 0;
     }
+
     images[image_index].style.display = "block";
+    // dots[image_index].style.background = "red";
+    dots[image_index].className += " dots-active";
     image_index++;
     console.log(image_index);
 
