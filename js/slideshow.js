55f9895d573a7c896989bda61ad1e64332b6783a
tahmid526628 <42012470+tahmid526628@users.noreply.github.com>
2020-07-01 13:14:41 +0600

    Update slideshow.js

--- js/slideshow.js
+++ js/slideshow.js
@@ -1,4 +1,4 @@
-var slide_index = 0;
+var image_index = 0;
 slide_show();
 
 function slide_show(){
@@ -9,12 +9,12 @@ function slide_show(){
         images[i].style.display = "none";
     }
 
-    if(slide_index >= images.length){
-        slide_index = 0;
+    if(image_index >= images.length){
+        image_index = 0;
     }
-    images[slide_index].style.display = "block";
-    slide_index++;
-    console.log(slide_index);
+    images[image_index].style.display = "block";
+    image_index++;
+    console.log(image_index);
 
 
     setTimeout(slide_show, 2000); // durtion 2 secs
