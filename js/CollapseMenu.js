8a11845f5c1694451823d77457249758353e795d
tahmid526628 <42012470+tahmid526628@users.noreply.github.com>
2020-07-02 19:00:16 +0600

    Add files via upload

---
+++ js/CollapseMenu.js
@@ -0,0 +1,17 @@
+var isMenuOpen = false;
+
+function collapseButtonActive(){
+    var navBar = document.getElementById("navbar");
+    var icon = document.getElementById("collapse-able-icon");
+    if(isMenuOpen){
+        navBar.style.display = "none";
+        icon.innerHTML = "&#9776";
+        console.log("menu close");
+        isMenuOpen = false;
+    }else{
+        navBar.style.display = "block";
+        icon.innerHTML = "&#9886;"
+        console.log("menu open");
+        isMenuOpen = true;
+    }
+}
